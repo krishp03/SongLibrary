6e30996a9d8bb51a2fdb34ca8ed391f29042c573
rolutowns <59771573+rolutowns@users.noreply.github.com>
2023-02-21 01:46:24 -0500

    Update PrimaryController.java

--- PrimaryController.java
+++ PrimaryController.java
@@ -49,10 +49,34 @@ public class PrimaryController {
         addYearField.setText("");
     }
 
+    private void showSong(){
+        if (songListView.getItems().size()==0){
+            titleLabel.setText("");
+            artistLabel.setText("");
+            albumLabel.setText("");
+            yearLabel.setText("");
+            return;
+        }
+        Song curr = songListView.getSelectionModel().getSelectedItem();
+        if(curr!=null) {
+            titleLabel.setText(curr.getName());
+            artistLabel.setText(curr.getArtist());
+            albumLabel.setText(curr.getAlbum());
+            yearLabel.setText(Integer.toString(curr.getYear()));
+        }
+    }
+
     public void buttonListener(ActionEvent e) {
         Button act = (Button) e.getSource();
         if (act==buttonAdd) screenPane.setRight(addPane);
-        else if(act==buttonEdit) screenPane.setRight(editPane);
+        else if(act==buttonEdit) {
+            screenPane.setRight(editPane);
+            Song s = songListView.getSelectionModel().getSelectedItem();
+            editSongField.setText(s.getName());
+            editArtistField.setText(s.getArtist());
+            editAlbumField.setText(s.getAlbum());
+            editYearField.setText(Integer.toString(s.getYear()));
+        }
         else if(act==buttonDelete || act==buttonConfirmAdd || act==buttonUpdate) {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "");
             if (act==buttonDelete){
@@ -68,8 +92,25 @@ public class PrimaryController {
                     Song s = new Song(addSongField.getText(), addArtistField.getText(), addAlbumField.getText(), Integer.parseInt(addYearField.getText()));
                     songListView.getItems().add(s);
                     Collections.sort(songListView.getItems());
+                    songListView.getSelectionModel().select(s);
                     clearAdd();
                 }
+                else if(act==buttonDelete){
+                    int i = songListView.getSelectionModel().getSelectedIndex();
+                    songListView.getItems().remove(songListView.getSelectionModel().getSelectedItem());
+                    if (i>=songListView.getItems().size()){
+                        if (i!=0) i--;
+                    }
+                    songListView.getSelectionModel().select(i);
+                }
+                else{
+                    Song s = new Song(editSongField.getText(), editArtistField.getText(), editAlbumField.getText(), Integer.parseInt(editYearField.getText()));
+                    songListView.getItems().remove(songListView.getSelectionModel().getSelectedItem());
+                    songListView.getItems().add(s);
+                    Collections.sort(songListView.getItems());
+                    songListView.getSelectionModel().select(s);
+                }
+                showSong();
                 screenPane.setRight(mainPane);
             }
         }
