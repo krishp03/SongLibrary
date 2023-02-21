247b7a5e7d78f0ae4bf6e805c205426f9bce8830
rolutowns <59771573+rolutowns@users.noreply.github.com>
2023-02-21 00:38:42 -0500

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,57 +1,78 @@
 package org.openjfx;
 
-public class Song {
-    String name;
-    String artist;
-    String album;
-    int year;
-    public Song(String name, String artist){
-        this.name = name;
-        this.artist = artist;
-        this.album="";
-        this.year=0;
-    }
-    public Song(String name, String artist, String album){
-        this.name = name;
-        this.artist = artist;
-        this.album=album;
-        this.year=0;
-    }
-    public Song(String name, String artist, int year){
-        this.name = name;
-        this.artist = artist;
-        this.album="";
-        this.year=year;
-    }
+public class Song implements Comparable<Song>{
+
+    private String name;
+    private String artist;
+    private String album;
+    private int year;
+
     public Song(String name, String artist, String album, int year){
+        if(name.isBlank() || artist.isBlank()){
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.artist = artist;
-        this.album=album;
-        this.year=year;
+        this.album = album;
+        this.year = year;
     }
+
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
+
     public String getArtist() {
         return artist;
     }
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
+
     public String getAlbum() {
         return album;
     }
-    public void setAlbum(String album) {
-        this.album = album;
-    }
+
     public int getYear() {
         return year;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
     public void setYear(int year) {
         this.year = year;
     }
-}
 
+    @Override
+    // Compare by name first, and if names are duplicates, compare by artist
+    public int compareTo(Song s) {
+        if(name.compareTo(s.getName()) != 0){
+            return name.toLowerCase().compareTo(s.getName().toLowerCase());
+        }
+        return artist.toLowerCase().compareTo(s.getArtist().toLowerCase());
+    }
+
+    @Override
+    // Equivalent if name AND artist are same(case INsensitive)
+    public boolean equals(Object o){
+        if(o == this){
+            return true;
+        }
+
+        if(!(o instanceof Song)){
+            return false;
+        }
+
+        Song s = (Song)o;
+
+        return s.getName().equalsIgnoreCase(name)
+                && s.getArtist().equalsIgnoreCase(artist);
+    }
+
+}
