ca100cfdf8676bf830eddb79adc2b2e8e96b9378
rolutowns <59771573+rolutowns@users.noreply.github.com>
2023-02-21 00:40:16 -0500

    Update PrimaryController.java

--- PrimaryController.java
+++ PrimaryController.java
@@ -1,76 +1,81 @@
-public class Song implements Comparable<Song>{
+package org.openjfx;
 
-    private String name;
-    private String artist;
-    private String album;
-    private int year;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-    public Song(String name, String artist, String album, int year){
-        if(name.isBlank() || artist.isBlank()){
-            throw new IllegalArgumentException();
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+
+public class PrimaryController {
+
+    @FXML BorderPane screenPane;
+    @FXML GridPane addPane;
+    @FXML GridPane editPane;
+    @FXML GridPane mainPane;
+    @FXML TextField addSongField;
+    @FXML TextField addArtistField;
+    @FXML TextField addAlbumField;
+    @FXML TextField addYearField;
+    @FXML TextField editSongField;
+    @FXML TextField editArtistField;
+    @FXML TextField editAlbumField;
+    @FXML TextField editYearField;
+    @FXML Button buttonConfirmAdd;
+    @FXML Button buttonCancelAdd;
+    @FXML Button buttonUpdate;
+    @FXML Button buttonCancelEdit;
+    @FXML Button buttonAdd;
+    @FXML Button buttonDelete;
+    @FXML Button buttonEdit;
+    @FXML Label titleLabel;
+    @FXML Label artistLabel;
+    @FXML Label albumLabel;
+    @FXML Label yearLabel;
+    @FXML ListView<Song> songListView = new ListView<Song>();
+
+    private void clearAdd(){
+        addSongField.setText("");
+        addArtistField.setText("");
+        addAlbumField.setText("");
+        addYearField.setText("");
+    }
+
+    public void buttonListener(ActionEvent e) {
+        Button act = (Button) e.getSource();
+        if (act==buttonAdd) screenPane.setRight(addPane);
+        else if(act==buttonEdit) screenPane.setRight(editPane);
+        else if(act==buttonDelete || act==buttonConfirmAdd || act==buttonUpdate) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "");
+            if (act==buttonDelete){
+                alert.setContentText("Confirm Delete?");
+            }else if (act==buttonConfirmAdd){
+                alert.setContentText("Confirm Add?");
+            }else{
+                alert.setContentText("Confirm Edit?");
+            }
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                if(act==buttonConfirmAdd){
+                    Song s = new Song(addSongField.getText(), addArtistField.getText(), addAlbumField.getText(), Integer.parseInt(addYearField.getText()));
+                    songListView.getItems().add(s);
+                    Collections.sort(songListView.getItems());
+                    clearAdd();
+                }
+                screenPane.setRight(mainPane);
+            }
         }
-        this.name = name;
-        this.artist = artist;
-        this.album = album;
-        this.year = year;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public String getAlbum() {
-        return album;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    @Override
-    // Compare by name first, and if names are duplicates, compare by artist
-    public int compareTo(Song s) {
-        if(name.compareTo(s.getName()) != 0){
-            return name.toLowerCase().compareTo(s.getName().toLowerCase());
+        else if(act==buttonCancelAdd || act==buttonCancelEdit) {
+            if(act==buttonCancelAdd) clearAdd();
+            screenPane.setRight(mainPane);
         }
-        return artist.toLowerCase().compareTo(s.getArtist().toLowerCase());
     }
-
-    @Override
-    // Equivalent if name AND artist are same(case INsensitive)
-    public boolean equals(Object o){
-        if(o == this){
-            return true;
-        } 
-
-        if(!(o instanceof Song)){
-            return false;
-        }
-
-        Song s = (Song)o;
-
-        return s.getName().equalsIgnoreCase(name)
-        && s.getArtist().equalsIgnoreCase(artist);
-    }
-
 }
