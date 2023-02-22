a7c9e2c0914832d929eae49a9bfa9d16edd34fbc
rolutowns <59771573+rolutowns@users.noreply.github.com>
2023-02-21 19:14:31 -0500

    Update PrimaryController.java

--- PrimaryController.java
+++ PrimaryController.java
@@ -1,21 +1,26 @@
+/*
+ * Song Library Design & Implementation with GUI
+ * By Krish Patel and Roshan Varadhan
+ */
 package org.openjfx;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
+import java.io.*;
+import java.net.URL;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 
-public class PrimaryController {
+public class PrimaryController implements Initializable{
 
     @FXML BorderPane screenPane;
     @FXML GridPane addPane;
@@ -51,10 +56,10 @@ public class PrimaryController {
 
     private void showSong(){
         if (songListView.getItems().size()==0){
-            titleLabel.setText("");
-            artistLabel.setText("");
-            albumLabel.setText("");
-            yearLabel.setText("");
+            titleLabel.setText("Song Library");
+            artistLabel.setText("By Krish Patel and Roshan Varadhan");
+            albumLabel.setText("CS 213");
+            yearLabel.setText("2023");
             return;
         }
         Song curr = songListView.getSelectionModel().getSelectedItem();
@@ -62,11 +67,11 @@ public class PrimaryController {
             titleLabel.setText(curr.getName());
             artistLabel.setText(curr.getArtist());
             albumLabel.setText(curr.getAlbum());
-            yearLabel.setText(Integer.toString(curr.getYear()));
+            yearLabel.setText(curr.getYear());
         }
     }
 
-    public void buttonListener(ActionEvent e) {
+    public void buttonListener(ActionEvent e) throws IOException {
         Button act = (Button) e.getSource();
         if (act==buttonAdd) screenPane.setRight(addPane);
         else if(act==buttonEdit) {
@@ -75,7 +80,7 @@ public class PrimaryController {
             editSongField.setText(s.getName());
             editArtistField.setText(s.getArtist());
             editAlbumField.setText(s.getAlbum());
-            editYearField.setText(Integer.toString(s.getYear()));
+            editYearField.setText(s.getYear());
         }
         else if(act==buttonDelete || act==buttonConfirmAdd || act==buttonUpdate) {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "");
@@ -89,10 +94,17 @@ public class PrimaryController {
             Optional<ButtonType> result = alert.showAndWait();
             if (result.isPresent() && result.get() == ButtonType.OK) {
                 if(act==buttonConfirmAdd){
-                    Song s = new Song(addSongField.getText(), addArtistField.getText(), addAlbumField.getText(), Integer.parseInt(addYearField.getText()));
-                    songListView.getItems().add(s);
-                    Collections.sort(songListView.getItems());
-                    songListView.getSelectionModel().select(s);
+                    Song s = createSong(addSongField.getText(), addArtistField.getText(), addAlbumField.getText(), addYearField.getText());
+                    if(s!=null){
+                        if(!songListView.getItems().contains(s)){
+                            songListView.getItems().add(s);
+                            Collections.sort(songListView.getItems());
+                            songListView.getSelectionModel().select(s);
+                        } else{
+                            Alert a = new Alert(Alert.AlertType.ERROR, "This song already exists in the Library!!");
+                            a.showAndWait();
+                        }
+                    }
                     clearAdd();
                 }
                 else if(act==buttonDelete){
@@ -104,12 +116,18 @@ public class PrimaryController {
                     songListView.getSelectionModel().select(i);
                 }
                 else{
-                    Song s = new Song(editSongField.getText(), editArtistField.getText(), editAlbumField.getText(), Integer.parseInt(editYearField.getText()));
-                    songListView.getItems().remove(songListView.getSelectionModel().getSelectedItem());
-                    songListView.getItems().add(s);
-                    Collections.sort(songListView.getItems());
-                    songListView.getSelectionModel().select(s);
+                    Song s = createSong(editSongField.getText(), editArtistField.getText(), editAlbumField.getText(), editYearField.getText());
+                    if(s != null && (!songListView.getItems().contains(s) || songListView.getSelectionModel().getSelectedItem().equals(s))){
+                        songListView.getItems().remove(songListView.getSelectionModel().getSelectedItem());
+                        songListView.getItems().add(s);
+                        Collections.sort(songListView.getItems());
+                        songListView.getSelectionModel().select(s);
+                    } else if(s != null){
+                        Alert a = new Alert(Alert.AlertType.ERROR, "This song already exists in the Library");
+                        a.showAndWait();
+                    }
                 }
+                persistence();
                 showSong();
                 screenPane.setRight(mainPane);
             }
@@ -118,5 +136,88 @@ public class PrimaryController {
             if(act==buttonCancelAdd) clearAdd();
             screenPane.setRight(mainPane);
         }
+    }
+
+    private void persistence() throws IOException {
+        try{
+            FileWriter f = new FileWriter("songs.txt", false);
+            f.write("");
+            f.close();
+            BufferedWriter b = new BufferedWriter(new FileWriter("songs.txt", true));
+            for (Song s : songListView.getItems()) {
+                b.write(s.getName() + "|" + s.getArtist() + "|" + s.getAlbum() + "|" + s.getYear());
+                b.write("\n");
+            }
+            b.close();
+        } catch (IOException e){
+        }
+
+    }
+    private Song createSong(String name, String artist, String album, String year) {
+        try{
+            if(name.isBlank() || artist.isBlank()){
+                Alert a = new Alert(Alert.AlertType.ERROR, "A song needs to have at least a name and artist");
+                a.showAndWait();
+                return null;
+            }
+            if(name.contains("|") || artist.contains("|")){
+                Alert a = new Alert(Alert.AlertType.ERROR, "No Vertical Bars Allowed!!");
+                a.showAndWait();
+                return null;
+            }
+            if(!album.isBlank() && album.contains("|")){
+                Alert a = new Alert(Alert.AlertType.ERROR, "No Vertical Bars Allowed!!");
+                a.showAndWait();
+                return null;
+            }
+            Song s = new Song(name, artist, album, year);
+            return s;
+        } catch(Exception e){
+            Alert a = new Alert(Alert.AlertType.ERROR, "If you include a year, it must be a positive integer");
+            a.showAndWait();
+            return null;
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+            BufferedReader songLibrary = new BufferedReader(new FileReader("songs.txt"));
+            String part = songLibrary.readLine();
+            while (part!=null){
+                String fields[]=part.split("\\|");
+                String name=fields[0];
+                String artist=fields[1];
+                String album=fields[2];
+                String year=fields[3];
+                if(album.equals("null")){
+                    album="";
+                }if(year.equals("null")){
+                    year="";
+                }
+                Song s=createSong(name, artist, album, year);
+                songListView.getItems().add(s);
+                part = songLibrary.readLine();
+            }
+            songLibrary.close();
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(songListView.getItems().size()>0){
+            songListView.getSelectionModel().selectFirst();
+        }
+        showSong();
+        songListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Song>(){
+
+            @Override
+            public void changed(ObservableValue<? extends Song> arg0, Song arg1, Song arg2) {
+                showSong();
+            }
+
+        });
+
     }
 }
